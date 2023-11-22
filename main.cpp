ee46d39928d389e9a9737d0a941c7038ce8b7ed5
javicensaez <jvicente@deusto.es>
2023-11-22 16:03:47 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,41 +7,18 @@
 #include "Grove_LCD_RGB_Backlight.h"
 
 #define WAIT_TIME_MS 500 
-#define R0 100000.0
-#define R 100000.0
-#define Beta 4250.0
-#define T0 298.3
-#define Vcc 5.0
-#define Vref 3.3
-
-AnalogIn ntc(A0); //Entrada analogica para leer la tensióo de la NTC
-Grove_LCD_RGB_Backlight rgbLCD(PB_9,PB_8); //El switch de la placa de grove hay que seleccionarlo en 5V para que se ilumine bien
-
-
-
-float temp; // variable para guardar el valor de la temperatura
-char cadena[16]; //Cadena para generar el texto a ver en el LCD
- 
-float calculaTemp(float dato){ //Función que calcula la temperatura
-    float vNTC; //Tensión del divisor de tensión entre la NTC y la resistencia Fija
-    float rNTC; //Resistencia de la NTC a la temperatura ambiente
-    vNTC=dato*Vref; //La tensión de referencia de la tarjeta es de 3.3, el read devuelve un dato entre 0 y 1.
-    rNTC=((Vcc*R)/vNTC)-R; //Calcula la resistencia de la NTC, La tensión de VCC es 5V ya que es el voltaje que pasa el cable al sensor. Para que el LCD funcione.
-    return (Beta/(log(rNTC/R0)+(Beta/T0)))-273.3;
-}
+DigitalOut led1(LED1);
+Grove_LCD_RGB_Backlight rgbLCD(PB_9,PB_8);
 
 int main()
 {
-    rgbLCD.setRGB(0xff, 0xff, 0xff);                
+    printf("This is the bare metal blinky example running on Mbed OS %d.%d.%d.\n", MBED_MAJOR_VERSION, MBED_MINOR_VERSION, MBED_PATCH_VERSION);
 
     while (true)
     {
-        rgbLCD.locate(0,0); //Lo siguiente que se mande al LCD en primera fila, primera columna
-        rgbLCD.print("La temp es:"); //Escribe un texto fijo
-        temp=calculaTemp(ntc.read());   //Convierte el valor del adc en temperatura
-        sprintf(cadena,"%.2f C",temp); //Calcula la cadena para visualizar la temperatura en el LCD
-        rgbLCD.locate(0,1); //Lo siguiente lo escribe en la segunda fila, primera columna
-        rgbLCD.print(cadena); //Manda el texto formateado al LCD
-        thread_sleep_for(WAIT_TIME_MS); //Espera medio segundo.
+        rgbLCD.setRGB(0xff, 0xff, 0xff);                 //set the color 
+        rgbLCD.print("Hello World!");
+        led1 = !led1;
+        thread_sleep_for(WAIT_TIME_MS);
     }
 }
